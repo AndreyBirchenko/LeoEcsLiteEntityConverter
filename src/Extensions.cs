3bf71cad9d1442a70a96c260f3c37f533472c52e
AndreyBirchenko <62035054+AndreyBirchenko@users.noreply.github.com>
2023-03-04 11:54:21 +0300

    Fix bug with multiple conversion

--- src/Extensions.cs
+++ src/Extensions.cs
@@ -1,14 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 
 using Leopotam.EcsLite;
+
+using UnityEngine;
 
 namespace AB_Utility.FromSceneToEntityConverter
 {
     public static class Extensions
     {
+        private static HashSet<EcsWorld> _dirtyWorlds = new HashSet<EcsWorld>();
         public static IEcsSystems ConvertScene(this IEcsSystems systems)
         {
             var world = systems.GetWorld();
+            if (_dirtyWorlds.Contains(world))
+            {
+               throw new Exception("You cannot convert systems with the same world twice <a href=\"https://safe-clove-478.notion.site/You-cannot-convert-systems-with-the-same-world-twice-6f4c638c18e74b0b91c65cd0c7f9dc7b\">more info</a>" );
+            }
+
+            _dirtyWorlds.Add(world);
             var containers = UnityEngine.Object.FindObjectsOfType<ComponentsContainer>(true);
 
             for (int i = 0; i < containers.Length; i++)
