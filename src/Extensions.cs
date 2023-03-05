31d3f9e0827c070696f0ed27e705ea80f7900516
AndreyBirchenko <62035054+AndreyBirchenko@users.noreply.github.com>
2023-03-05 08:19:55 +0300

    Add debug directive

--- src/Extensions.cs
+++ src/Extensions.cs
@@ -13,12 +13,14 @@ namespace AB_Utility.FromSceneToEntityConverter
         public static IEcsSystems ConvertScene(this IEcsSystems systems)
         {
             var world = systems.GetWorld();
+#if DEBUG
             if (_dirtyWorlds.Contains(world))
             {
-               throw new Exception("You cannot convert systems with the same world twice <a href=\"https://safe-clove-478.notion.site/You-cannot-convert-systems-with-the-same-world-twice-6f4c638c18e74b0b91c65cd0c7f9dc7b\">more info</a>" );
+                throw new Exception("You cannot convert systems with the same world twice <a href=\"https://safe-clove-478.notion.site/You-cannot-convert-systems-with-the-same-world-twice-6f4c638c18e74b0b91c65cd0c7f9dc7b\">more info</a>" );
             }
 
             _dirtyWorlds.Add(world);
+#endif
             var containers = UnityEngine.Object.FindObjectsOfType<ComponentsContainer>(true);
 
             for (int i = 0; i < containers.Length; i++)
