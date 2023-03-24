151f082a3699ce6e0fb8c7533c322472eabba8d7
AndreyBirchenko <62035054+AndreyBirchenko@users.noreply.github.com>
2023-03-24 17:15:28 +0300

    Add instantiate and create entity functionality

---
+++ src/EcsConverter.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+using Leopotam.EcsLite;
+
+using UnityEngine;
+
+using Object = UnityEngine.Object;
+
+namespace AB_Utility.FromSceneToEntityConverter
+{
+    public static class EcsConverter
+    {
+        public static T InstantiateAndCreateEntity<T>
+        (T original, Vector3 position, Quaternion rotation, Transform parent,
+            EcsWorld world)
+            where T : UnityEngine.Object
+        {
+            var obj = Object.Instantiate(original, position, rotation, parent);
+            ConvertObject(obj as GameObject, world);
+            return obj;
+        }
+
+        public static T InstantiateAndCreateEntity<T>
+            (T original, Vector3 position, Quaternion rotation, EcsWorld world)
+            where T : UnityEngine.Object
+        {
+            var obj = Object.Instantiate(original, position, rotation);
+            ConvertObject(obj as GameObject, world);
+            return obj;
+        }
+
+        public static T InstantiateAndCreateEntity<T>
+            (T original, Transform parent, EcsWorld world, bool worldPositionStay)
+            where T : UnityEngine.Object
+        {
+            var obj = Object.Instantiate(original, parent, worldPositionStay);
+            ConvertObject(obj as GameObject, world);
+            return obj;
+        }
+
+        public static T InstantiateAndCreateEntity<T>
+            (T original, Transform parent, EcsWorld world)
+            where T : UnityEngine.Object
+        {
+            var obj = Object.Instantiate(original, parent);
+            ConvertObject(obj as GameObject, world);
+            return obj;
+        }
+
+        public static T InstantiateAndCreateEntity<T>
+            (T original, EcsWorld world)
+            where T : UnityEngine.Object
+        {
+            var obj = Object.Instantiate(original);
+            ConvertObject(obj as GameObject, world);
+            return obj;
+        }
+
+        internal static void ConvertContainer(ComponentsContainer container, EcsWorld world)
+        {
+            var destroyAfterConversion = container.DestroyAfterConversion;
+            var entity = world.NewEntity();
+            var packedEntity = world.PackEntityWithWorld(entity);
+
+            for (int j = 0; j < container.Converters.Length; j++)
+            {
+                var converter = container.Converters[j];
+                converter.Convert(packedEntity);
+
+                if (destroyAfterConversion)
+                {
+                    UnityEngine.Object.Destroy(converter);
+                }
+            }
+
+            if (destroyAfterConversion)
+            {
+                UnityEngine.Object.Destroy(container);
+            }
+        }
+
+        private static void ConvertObject(GameObject obj, EcsWorld world)
+        {
+            var container = obj.GetComponent<ComponentsContainer>();
+#if DEBUG
+            if (!container)
+            {
+                throw new Exception("You can not instantiate and create entity without components container");
+            }
+#endif
+
+            ConvertContainer(container, world);
+        }
+    }
+}
--- src/Extensions.cs
+++ src/Extensions.cs
@@ -5,18 +5,22 @@ using Leopotam.EcsLite;
 
 using UnityEngine;
 
+using Object = UnityEngine.Object;
+
 namespace AB_Utility.FromSceneToEntityConverter
 {
     public static class Extensions
     {
         private static HashSet<EcsWorld> _dirtyWorlds = new HashSet<EcsWorld>();
+
         public static IEcsSystems ConvertScene(this IEcsSystems systems)
         {
             var world = systems.GetWorld();
 #if DEBUG
             if (_dirtyWorlds.Contains(world))
             {
-                throw new Exception("You cannot convert systems with the same world twice <a href=\"https://safe-clove-478.notion.site/You-cannot-convert-systems-with-the-same-world-twice-6f4c638c18e74b0b91c65cd0c7f9dc7b\">more info</a>" );
+                throw new Exception(
+                    "You cannot convert systems with the same world twice <a href=\"https://safe-clove-478.notion.site/You-cannot-convert-systems-with-the-same-world-twice-6f4c638c18e74b0b91c65cd0c7f9dc7b\">more info</a>");
             }
 
             _dirtyWorlds.Add(world);
@@ -26,25 +30,7 @@ namespace AB_Utility.FromSceneToEntityConverter
             for (int i = 0; i < containers.Length; i++)
             {
                 var container = containers[i];
-                var destroyAfterConversion = container.DestroyAfterConversion;
-                var entity = world.NewEntity();
-                var packedEntity = world.PackEntityWithWorld(entity);
-
-                for (int j = 0; j < container.Converters.Length; j++)
-                {
-                    var converter = container.Converters[j];
-                    converter.Convert(packedEntity);
-                    
-                    if (destroyAfterConversion)
-                    {
-                        UnityEngine.Object.Destroy(converter);
-                    }
-                }
-
-                if (destroyAfterConversion)
-                {
-                    UnityEngine.Object.Destroy(container);
-                }
+                EcsConverter.ConvertContainer(container, world);
             }
 
             return systems;
